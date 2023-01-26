6b0d41807e8e808a86f5659bcc501c62e04a2ecc
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-26 15:46:50 +0200

    feat(Todos): build the main function to add tasks

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todos = () => {
+    const [todo, setTodo] = useState("");
+
+    const handleChage = (e) =>{
+         setTodo(e.target.value);
+    };
+console.log("todo text", todo);
   return (
-    <div>Todos</div>
+    <div className='addTodos'>
+        <input
+          type="text"
+          onClick={(e)=> handleChage(e)}
+          className='todo-input'
+          />
+          
+        <button className="add-btn">
+            Add 
+        </button>
+    </div>
   )
 }
 
