6fa3f02c1c799739491ad9179d72a6f4235aefca
Tarek994 <ghali.tarek.123@hotmail.com>
2023-01-27 17:29:14 +0200

    feat(Todos): reorganize the code and add motio from framer-motion

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,8 @@
-import React, { useRef, useState } from 'react'
-import { addTodos, removeTodos, updateTodos,completeTodos } from "../redux/reducer";
-import { connect } from 'react-redux';
-
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
 
 const mapStateToProps = (state) => {
   return {
@@ -12,86 +13,49 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return {
     addTodo: (obj) => dispatch(addTodos(obj)),
-    removeTodos: (id) => dispatch(removeTodos(id)),
-    updateTodo:(obj) => dispatch(updateTodos(obj)),
-    completeTodo:(id) => dispatch(completeTodos(id)),
   };
 };
 
-
 const Todos = (props) => {
-    const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState("");
 
-    const inputRef = useRef(true);
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
 
-    const changeFocus = () =>{
-      inputRef.current.disabled = false;
-      inputRef.current.focus();
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
     }
-
-    const update = (id,value,e) =>{
-
-      if(e.which === 13){
-        //
-        props.updateTodo({id, item:value });
-        inputRef.current.disabled = true;
-      }
-    }
-
-    const handleChage = (e) =>{
-         setTodo(e.target.value);
-    };
- console.log("props from store", props);
-
-
+  };
+  //console.log("props from store", props);
   return (
-    <div className='addTodos'>
-        <input
-          type="text"
-          onClick={(e)=> handleChage(e)}
-          className='todo-input'
-          />
-          
-        <button
-          className="add-btn"
-          onClick={() =>
-            props.addTodo({
-            id: Math.floor(Math.random() * 1000),
-            item: todo,
-            completed:false,
-        })}
-        >
-            Add 
-        </button>
-        <br/>
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="todo-input"
+        value={todo}
+      />
 
-        <ul>
-          {
-            props.todos.map((item) => {
-              return <li key={item.id}>
-                {item.item}
-                <textarea
-                ref={inputRef}
-                disabled={inputRef}
-                defaultValue={item.item}
-                onKeyPress={(e)=>update(item.id, inputRef.current.value, e)}
-                />
-                  
-                <button
-                onClick={() =>changeFocus() }
-                >Edit
-                </button>
-                <button
-                onClick={() => props.completeTodo(item.id) }>
-                complete
-                </button>
-                 <button onClick={() => props.removeTodo(item.id)}>Delete</button>{" "}
-                </li>;
-            })
-          }
-        </ul>
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="add-btn"
+        onClick={() => add()}
+      >
+        <GoPlus />
+      </motion.button>
+      <br />
     </div>
-  )
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Todos);
+  );
+};
+//we can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
